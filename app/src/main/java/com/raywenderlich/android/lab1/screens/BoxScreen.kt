2e4e62dda388cf1976647cbcfce1f21a893c26d5
Yana-Bordovich <98753081+Yana-Bordovich@users.noreply.github.com>
2024-02-09 14:19:52 +0300

    Initial project

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/BoxScreen.kt
@@ -0,0 +1,47 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.raywenderlich.android.lab1.R
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BoxScreen() {
+    MyBox()
+
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MyBox(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(id = R.string.text_top_start),
+            fontSize = 22.sp,
+            modifier = contentModifier.then(Modifier.align(Alignment.TopStart))
+        )
+        Text(
+            text = stringResource(id = R.string.text_center),
+            fontSize = 22.sp,
+            modifier = contentModifier.then(Modifier.align(Alignment.Center))
+        )
+        Text(
+            text = stringResource(id = R.string.text_bottom_end),
+            fontSize = 22.sp,
+            modifier = contentModifier.then(Modifier.align(Alignment.BottomEnd))
+        )
+    }
+}
