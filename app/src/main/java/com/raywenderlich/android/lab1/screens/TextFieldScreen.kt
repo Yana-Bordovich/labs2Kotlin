220faac02ebe6605f7ba5a94a9099a903465bfd7
Yana-Bordovich <98753081+Yana-Bordovich@users.noreply.github.com>
2024-02-10 18:05:14 +0300

    Initial project

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
